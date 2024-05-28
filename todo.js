60ef9a73debc7d2739fdfe61154494e998441f3d
Marcin Wos <marcin.wos@cdq.com>
2024-05-28 20:54:58 +0200

    small refactor

--- todo.js
+++ todo.js
@@ -1,23 +1,11 @@
-const tasks = [];
-
 const taskNameInput = document.getElementById("taskContent");
 const taskList = document.getElementById("taskList");
 
-const createTaskElement = ({ name, isCompleted }) => {
-  const taskElement = document.createElement("li");
-  taskElement.textContent = name;
-  if (isCompleted) taskElement.classList.add("styleComplete");
-
-  return taskElement;
-};
+let tasks = [];
 
 const printTasks = () => {
   const taskElements = tasks.map(createTaskElement);
-  taskList.replaceChildren(taskElements);
-};
-
-const clearInput = () => {
-  taskNameInput.value = "";
+  taskList.replaceChildren(...taskElements);
 };
 
 const addTask = () => {
@@ -29,36 +17,33 @@ const addTask = () => {
   printTasks();
 };
 
-function saveToLocalStorage() {
-  const tasksArray = [];
-  const taskItems = taskList.getElementsByTagName("li");
+const createTaskElement = ({ name, isCompleted }) => {
+  const taskElement = document.createElement("li");
+  taskElement.textContent = name;
+  taskElement.addEventListener("click", toggleCompletionState(taskElement));
 
-  for (let i = 0; i < taskItems.length; i++) {
-    tasksArray.push(taskItems[i].textContent);
-  }
+  if (isCompleted) taskElement.classList.add("styleComplete");
 
-  localStorage.setItem("tasks", JSON.stringify(tasksArray));
-}
+  return taskElement;
+};
 
-function loadFromLocalStorage() {
-  const tasksArray = JSON.parse(localStorage.getItem("tasks"));
-  if (tasksArray) {
-    tasksArray.forEach((newItem) => {
-      const newTask = document.createElement("li");
-      newTask.textContent = newItem;
-      taskList.appendChild(newTask);
-    });
-  }
-  // localStorage.setItem('tasks', JSON.stringify(tasksArray))
-}
+const toggleCompletionState = (task) => () => {
+  task.classList.toggle("styleComplete");
+};
+
+const clearInput = () => {
+  taskNameInput.value = "";
+};
+
+const loadFromLocalStorage = () => {
+  tasks = JSON.parse(localStorage.getItem("tasks")) ?? [];
+};
+
+const saveToLocalStorage = () => {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
 
 loadFromLocalStorage();
-
-var allLi = document.querySelectorAll("li");
-for (const li of allLi) {
-  li.addEventListener("click", () => {
-    li.classList.toggle("styleComplete");
-  });
-}
+printTasks();
 
 window.addTask = addTask;
